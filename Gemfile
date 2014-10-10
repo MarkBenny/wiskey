7f5f065165524f58f2eebe2c9c42abc4f78c7d62
MarkBenny <1markbenny@gmail.com>
2014-10-10 11:35:53 +1100

    gag

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'haml'
 gem 'bcrypt'
 
 
+
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,9 @@ module ApplicationHelper
 
 
 	def intellinav
+    
     nav = ''
+    
     if @current_user.present?
       nav += "<li>Hello #{ @current_user.name }</li> "
       nav += "<li>" + link_to('All users', users_path) + "</li>"
@@ -16,6 +18,8 @@ module ApplicationHelper
       nav += "<li>#{ link_to('Sign up', new_user_path) }</li> "
       nav += "<li>#{ link_to('Sign in', login_path) }</li>"
     end
+    
     nav
+    
   end
 end
